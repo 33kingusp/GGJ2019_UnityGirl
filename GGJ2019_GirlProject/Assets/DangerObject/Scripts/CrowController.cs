54509ccbf5994796a27423c91bf0aa127a7186d9
MYCOMPUTER\33kingusp <33kingusp>
2019-01-26 15:46:43 +0900

    カラスの処理の追加

---
+++ GGJ2019_GirlProject/Assets/DangerObject/Scripts/CrowController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowController : MonoBehaviour
+{
+    IEnumerator moveEnumerator = null;
+    private bool isMoveing;
+
+
+    private void Update()
+    {
+        FindGirl();
+    }
+
+    private void FindGirl()
+    {
+        RaycastHit hit = new RaycastHit();
+        if (Physics.SphereCast(transform.position, 1.0f, Vector3.down, out hit))
+        {
+            GirlProvider girl = hit.collider.GetComponent<GirlProvider>();
+            if (girl)
+            {
+                
+            }
+        }
+    }
+
+    public void Move(Vector3 position, float delay)
+    {
+        if(isMoveing)
+            StopCoroutine(moveEnumerator);
+
+        moveEnumerator = movePositon(position, delay);
+        StartCoroutine(moveEnumerator);
+    }
+
+    private IEnumerator movePositon(Vector3 position, float delay)
+    {
+        isMoveing = true;
+        Vector3 oldPosition = transform.position;
+        float t = 0;
+
+        do
+        {
+            transform.position = Vector3.Lerp(oldPosition, position, t / delay);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        while (t <= delay) ;
+        transform.position = position;
+        isMoveing = false;
+        yield break;
+    }
+}
---
+++ GGJ2019_GirlProject/Assets/DangerObject/Scripts/PoleController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleController : DangerObjectController
+{
+    [SerializeField] private PoleController nextPole;
+    [SerializeField] private CrowController crow;
+
+    public void MoveCrow()
+    {
+        crow.Move(nextPole.transform.position + Vector3.up * 4, 5f);
+    }
+}
