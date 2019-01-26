c02fd2d8389754d484a12edaa73a8c0e7fe606e6
MYCOMPUTER\33kingusp <33kingusp>
2019-01-26 17:24:31 +0900

    カラスのアニメーションの追加

--- GGJ2019_GirlProject/Assets/DangerObject/Scripts/CrowController.cs
+++ GGJ2019_GirlProject/Assets/DangerObject/Scripts/CrowController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CrowController : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] private PoleController startPole;
     private IEnumerator moveEnumerator = null;
     private bool isMoveing;
@@ -26,6 +27,7 @@ public class CrowController : MonoBehaviour
             GirlProvider girl = hit.collider.GetComponent<GirlProvider>();
             if (girl)
             {
+                // != girl.GetGirlMoveState();
                 girl.ReverseMoveDirection();
             }
         }
@@ -43,8 +45,13 @@ public class CrowController : MonoBehaviour
     private IEnumerator MovePositon(PoleController pole, float delay)
     {
         isMoveing = true;
+        animator.SetBool("isFrying", true);
         Vector3 oldPosition = transform.position;
         Vector3 position = pole.transform.position + Vector3.up * 4;
+
+        if (position.x - oldPosition.x <= 0) transform.eulerAngles = Vector3.zero;
+        else transform.eulerAngles = Vector3.up * 180;
+
         float t = 0;
 
         do
@@ -56,6 +63,7 @@ public class CrowController : MonoBehaviour
         while (t <= delay) ;
         transform.position = position;
         isMoveing = false;
+        animator.SetBool("isFrying", false);
         pole.StopCrow(this);
         yield break;
     }
