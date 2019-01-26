096f26415aedd14c34fd66245e6982dfc4e5d378
nuno <nenowawon@gmail.com>
2019-01-26 12:48:10 +0900

    幼女が坂道を移動できるように変更。

--- GGJ2019_GirlProject/Assets/Girls/Scripts/GirlMover.cs
+++ GGJ2019_GirlProject/Assets/Girls/Scripts/GirlMover.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 public class GirlMover : MonoBehaviour
 {
 
-    private bool isGround;
+    /// <summary>
+    /// 現在坂の上かどうか
+    /// </summary>
+    private bool isSlope = false;
 
     [SerializeField]
     private float sideMoveSpeed = 5f;
@@ -17,6 +20,15 @@ public class GirlMover : MonoBehaviour
     private MoveState currentMoveState = MoveState.RIGHT;
 
     private Rigidbody myRigidbody;
+
+    [SerializeField]
+    private float boxCastRayMargine = 0.4f;
+
+    private int slopeLayer = 9;
+
+    private int groundLayer = 8;
+
+    private int girlLayer = 10;
 
     private enum MoveState
     {
@@ -34,23 +46,35 @@ public class GirlMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!JudgeIsGround())
+        // 坂の動きをする
+        if (MoveSlope())
         {
-            FallMove();
-            return;
+            
+        }
+        else
+        {
+            var groundObject = JudgeIsGround();
+            // 接地していない場合
+            if (!groundObject)
+            {
+                Rotate(Quaternion.identity);
+                FallMove();
+                return;
+            }
         }
 
+        // 壁に衝突したかどうかを取得し、衝突時動作をする
         ReverseSideMove();
 
         SideMove();
-        
+
     }
 
     private void SideMove()
     {
         var movePos = transform.position;
 
-        movePos.x += sideMoveSpeed * Time.deltaTime* (int)currentMoveState;
+        movePos += GetMoveSideDirection() * sideMoveSpeed * Time.deltaTime;
 
         transform.position = movePos;
     }
@@ -60,18 +84,21 @@ public class GirlMover : MonoBehaviour
         // 現在の進んでいる方向にrayを設定する
         var ray = new Ray(transform.position, GetMoveSideDirection());
 
-        float rayDistance = (transform.lossyScale.x/2)+0.01f;
+        // 通常の床のみ衝突判定を取る
+        int layerMask = 1 << groundLayer | 1 << girlLayer;
 
-        if (Physics.Raycast(ray, rayDistance))
+        float rayDistance = (transform.lossyScale.x / 2) + 0.01f;
+
+        if (Physics.Raycast(ray, rayDistance, layerMask))
         {
             switch (currentMoveState)
             {
                 case MoveState.RIGHT:
-                    currentMoveState = MoveState.LEFT;
+                    ChangeMoveState(MoveState.LEFT);
                     break;
 
                 case MoveState.LEFT:
-                    currentMoveState = MoveState.RIGHT;
+                    ChangeMoveState(MoveState.RIGHT);
                     break;
             }
         }
@@ -79,7 +106,7 @@ public class GirlMover : MonoBehaviour
 
     private void ChangeMoveState(MoveState moveState)
     {
-        
+        currentMoveState = moveState;
     }
 
     private void FallMove()
@@ -91,30 +118,86 @@ public class GirlMover : MonoBehaviour
         transform.position = movePos;
     }
 
-    private bool JudgeIsGround()
+    private void Rotate(Quaternion rotation)
+    {
+        transform.rotation = rotation;
+    }
+
+    private bool MoveSlope()
+    {
+        // 坂を取得する
+        var slopeObject = JudgeIsSlope();
+        // 坂だった場合
+        if (slopeObject)
+        {
+            isSlope = true;
+            Rotate(slopeObject.transform.rotation);
+
+            //Vector3 movepos = transform.position;
+            //movepos.y = slopeObject.transform.position.y + (transform.lossyScale.y / 2f) + (slopeObject.transform.lossyScale.y / 2f);
+
+            //transform.position = movepos;
+        }
+        else
+        {
+            isSlope = false;
+        }
+        return isSlope;
+    }
+
+    private GameObject JudgeIsGround()
     {
         // 自分の下の方にrayを飛ばす
-        var ray = new Ray(transform.position,-transform.up);
+        var ray = new Ray(transform.position, -transform.up);
 
         // rayの距離を設定
-        float rayDistance = (transform.lossyScale.y / 2);
-
-        //RaycastHit hit;
+        float rayDistance = (transform.lossyScale.y * 0.5f) - boxCastRayMargine;
 
         var rayOrigin = transform.position;
 
-        rayOrigin.y += 0.1f;
+        rayOrigin.y += 0.3f;
 
-        var isHit = Physics.BoxCast(rayOrigin, (transform.lossyScale)/2, -transform.up, transform.rotation);
+        // 通常の床のみ衝突判定を取る
+        int layerMask = 1 << groundLayer;
 
-        //Debug.Log(isHit);
+        RaycastHit hit;
+
+        var isHit = Physics.BoxCast(rayOrigin, (transform.lossyScale) / 2f, -transform.up, out hit, transform.rotation, rayDistance, layerMask);
 
         if (isHit)
         {
-            return true;
+            return hit.collider.gameObject;
         }
 
-        return false;
+        return null;
+    }
+
+    private GameObject JudgeIsSlope()
+    {
+        // 自分の下の方にrayを飛ばす
+        var ray = new Ray(transform.position, -transform.up);
+
+        // rayの距離を設定
+        float rayDistance = (transform.lossyScale.y * 0.5f);
+
+        RaycastHit hit;
+
+        var rayOrigin = transform.position;
+
+        // 坂のみ衝突判定を取る
+        int layerMask = 1 << slopeLayer;
+
+        rayOrigin.y += 0.1f;
+
+        var isHit = Physics.BoxCast(rayOrigin, (transform.lossyScale) *0.5f, -transform.up, out hit, transform.rotation, rayDistance, layerMask);
+
+        if (isHit)
+        {
+            Debug.Log(hit.collider.gameObject);
+            return hit.collider.gameObject;
+        }
+
+        return null;
     }
 
     private Vector3 GetMoveSideDirection()
