ccaa06f1f911ce29af5271b46ad1680f1c0bd5ba
MYCOMPUTER\33kingusp <33kingusp>
2019-01-26 12:49:07 +0900

    メソッドの追加

--- GGJ2019_GirlProject/Assets/DangerObject/Scripts/DangerObjectController.cs
+++ GGJ2019_GirlProject/Assets/DangerObject/Scripts/DangerObjectController.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class DangerObjectController : MonoBehaviour
 {
-
     private void OnTriggerEnter(Collider other)
     {
         GirlProvider girl =  other.GetComponent<GirlProvider>();
-        if(other != null)
+
+        if (other != null)
         {
             
         }
+    }
+
+    protected virtual void KillGirl(GirlProvider girlProvider)
+    {
+
     }
 }
