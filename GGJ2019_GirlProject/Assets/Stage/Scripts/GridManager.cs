d1eabc9c08753d08879222e90e2e5d2c1fb0d8d6
MYCOMPUTER\33kingusp <33kingusp>
2019-01-27 00:10:49 +0900

    設置ギミックが重ならないように実装

--- GGJ2019_GirlProject/Assets/Stage/Scripts/GridManager.cs
+++ GGJ2019_GirlProject/Assets/Stage/Scripts/GridManager.cs
@@ -12,20 +12,21 @@ public class GridManager
     /// <returns></returns>
     public static Vector3 GetGridPosition(Vector3 position)
     {
-        
+
         Vector3 gridPos = position;
 
         gridPos.x = Mathf.Floor(gridPos.x);
 
         gridPos.y = Mathf.Ceil(gridPos.y);
 
-        Debug.Log($"現在の座標={position},グリッド座標={gridPos}");
+        //Debug.Log($"現在の座標={position},グリッド座標={gridPos}");
 
         return gridPos;
     }
 
-    //private void Update()
-    //{
-    //    GetGridPosition(transform.position);
-    //}
+    public static bool IsEmptyGrid(Vector3 position)
+    {
+        position += Vector3.down * 0.5f + Vector3.right * 0.5f + Vector3.forward * 2f;
+        return (Physics.BoxCast(position, Vector3.one * 0.45f, Vector3.back));
+    }
 }
