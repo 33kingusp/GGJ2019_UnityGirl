6a90bd07e741954533bdcb32477d79c138b38faf
MYCOMPUTER\33kingusp <33kingusp>
2019-01-27 13:26:55 +0900

    カラスの挙動

--- GGJ2019_GirlProject/Assets/DangerObject/Scripts/DangerObjectController.cs
+++ GGJ2019_GirlProject/Assets/DangerObject/Scripts/DangerObjectController.cs
@@ -4,20 +4,5 @@ using UnityEngine;
 
 public class DangerObjectController : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        /*
-        Debug.Log("Collision");
-        GirlProvider girl = other.gameObject.GetComponent<GirlProvider>();
 
-        if (girl != null)
-            KillGirl(girl);
-            */
-    }
-
-    protected virtual void KillGirl(GirlProvider girlProvider)
-    {
-        Debug.Log("KillGirl : " + girlProvider);
-        girlProvider.Death();
-    }
 }
