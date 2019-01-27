5a554e63e6aeb636149001efa81485dfe8838be5
nuno <nenowawon@gmail.com>
2019-01-27 13:24:06 +0900

    幼女の死亡アニメーションを再生するように変更。

--- GGJ2019_GirlProject/Assets/Girls/Scripts/Provider.cs
+++ GGJ2019_GirlProject/Assets/Girls/Scripts/Provider.cs
@@ -16,12 +16,16 @@ public class Provider : MonoBehaviour
 
     private AudioSource audioSource;
 
+    private Animator animator;
+
     // Start is called before the first frame update
     protected virtual void Awake()
     {
         mover = GetComponent<Mover>();
         myCollider = GetComponent<Collider>();
         audioSource = GetComponent<AudioSource>();
+
+        animator = GetComponent<Animator>();
     }
 
     public void Death()
@@ -31,7 +35,9 @@ public class Provider : MonoBehaviour
         myCollider.enabled = false;
 
         audioSource.PlayOneShot(deathAudioClip);
-        // TODO: アニメーション関連の処理を追加
+
+        // 死亡アニメーションを再生
+        animator.SetBool("IsDeath",true);
 
         Destroy(gameObject, deathTimer);
     }
